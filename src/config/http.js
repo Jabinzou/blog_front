87b74035c281a4c9de42fae3848e1805e14b434a
jabin <jabin@wezhuiyi.com>
2018-08-11 16:09:49 +0800

    [feature]添加http配置

---
+++ src/config/http.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const config = {
+  baseURL: '/api',
+  headers: {'X-Requested-With': 'XMLHttpRequest'},
+  timeout: 10000
+};
+
+export default config;
---
+++ src/init.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import axios from 'axios';
+import config from './config/http';
+
+axios.defaults.headers.common['Cache-Control'] = 'private, no-store';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+const http = axios.create(config);
